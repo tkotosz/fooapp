520bfca4d3cb47802397649a543b1b0ff4b1daf0
tkotosz <tkotosz@inviqa.com>
2020-07-05 15:38:42 +0200

    fixup

--- build/build-phar.sh
+++ build/build-phar.sh
@@ -2,25 +2,16 @@
 
 set -e
 
-BOX_DIR="/tmp/box"
-
-mkdir -p ${BOX_DIR}
-
-# Install humbug/box
-composer --working-dir=${BOX_DIR} require humbug/box "^3.8" --no-interaction --no-progress --no-suggest
-
 cd build
 
 ./app-builder.php
 
 cd build-tmp
 
-composer install --no-dev
-${BOX_DIR}/vendor/bin/box compile
+composer install
+box compile
 
 cd ..
-
 rm -rf build-tmp
-rm -rf ${BOX_DIR}
 
 cd ..
